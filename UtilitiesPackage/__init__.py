ad5d9190b559dfa8b42294b5fc633be214ed6967
Noel <noel@idxtra.com>
2012-12-14 18:18:27 +0000

    WIP 2 14122012

--- UtilitiesPackage/__init__.py
+++ UtilitiesPackage/__init__.py
@@ -16,15 +16,14 @@ __all__ = [
 from UtilitiesPackage.File import package_contents
 
 def mayaImportAll( module ):
-	exec "from %s import *" % module
+	return "from %s import *" % module
 
 def mayaImport( module ):
 	"""
 		Trick to import and reload modules to avoid restarting maya
 		during debugging
 	"""
-	exec "import %s " % module
-	exec "reload( %s )" % module
+	return ("import %s " % module + "\n" + "reload( %s )" % module)
 
 def mayaFromImport( modulePath, module ):
 	"""
@@ -36,5 +35,5 @@ def mayaFromImport( modulePath, module ):
 
 for module in package_contents("UtilitiesPackage"):
 	if module != "__init__":
-		mayaImportAll( module )
+		exec mayaImportAll( module )
 
