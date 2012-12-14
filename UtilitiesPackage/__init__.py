518872c118153a2a4c712682c007a5620f9e6913
Noel <noel@idxtra.com>
2012-12-14 18:11:05 +0000

    Another WIP commit 14122012

--- UtilitiesPackage/__init__.py
+++ UtilitiesPackage/__init__.py
@@ -15,8 +15,26 @@ __all__ = [
 
 from UtilitiesPackage.File import package_contents
 
+def mayaImportAll( module ):
+	exec "from %s import *" % module
+
+def mayaImport( module ):
+	"""
+		Trick to import and reload modules to avoid restarting maya
+		during debugging
+	"""
+	exec "import %s " % module
+	exec "reload( %s )" % module
+
+def mayaFromImport( modulePath, module ):
+	"""
+		Trick to import and reload modules to avoid restarting maya
+		during debugging
+	"""
+	exec "from %s import %s" % (modulePath, module)
+	exec "reload( %s )" % module
+
 for module in package_contents("UtilitiesPackage"):
 	if module != "__init__":
-		execStr = "from %s import *" % module
-		exec execStr
+		mayaImportAll( module )
 
