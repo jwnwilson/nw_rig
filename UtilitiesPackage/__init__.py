924e7604687485348a9276fbd875ce03927d4555
Noel <noel@idxtra.com>
2012-12-18 18:46:05 +0000

    new import functionally is working but untested

--- UtilitiesPackage/__init__.py
+++ UtilitiesPackage/__init__.py
@@ -20,18 +20,15 @@ def mayaImportAll( module ):
 
 def mayaImport( module ):
 	"""
-		Trick to import and reload modules to avoid restarting maya
-		during debugging
+	return string to import module and reload it
 	"""
 	return ("import %s " % module + "\n" + "reload( %s )" % module)
 
 def mayaFromImport( modulePath, module ):
 	"""
-		Trick to import and reload modules to avoid restarting maya
-		during debugging
+	return string to import module and reload it
 	"""
-	exec "from %s import %s" % (modulePath, module)
-	exec "reload( %s )" % module
+	return ("from %s import %s" % (modulePath, module) + "\n" + "reload( %s )" % module) 
 
 for module in package_contents("UtilitiesPackage"):
 	if module != "__init__":
